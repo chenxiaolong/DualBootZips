bd1b1180ccfc78473d649266f6b7369b864bfc1e
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-01-21 15:07:20 -0500

    Remove DualBootDisable

--- create-zips.sh
+++ create-zips.sh
@@ -3,52 +3,6 @@
 cd "$(dirname ${0})"
 
 UPDATE_BINARY=""
-ROM_ZIP=""
-
-usage() {
-  echo "Usage: ${0} -z [any ROM zip file]"
-  exit 1
-}
-
-if [ ${#} -eq 0 ]; then
-  usage
-fi
-
-while [ ${#} -gt 0 ]; do
-  case ${1} in
-  -z)
-    shift
-    ROM_ZIP=${1}
-    shift
-    ;;
-  -h)
-    usage
-    ;;
-  *)
-    echo "Unrecognized argument: ${1}"
-    echo
-    usage
-    ;;
-  esac
-done
-
-if [ ! -f "${ROM_ZIP}" ]; then
-  echo "${ROM_ZIP} does not exist!"
-  exit 1
-fi
-
-rm -rf META-INF/
-unzip "${ROM_ZIP}" META-INF/com/google/android/update-binary
-
-#for i in $(find . -maxdepth 1 -type d ! -name .git ! -name . ! -name META-INF); do
-for i in DualBootDisable; do
-  i=$(basename ${i})
-  cd ${i}
-  rm -f ../${i}.zip
-  zip -r ../${i}.zip *
-  cd ..
-  zip ${i}.zip META-INF/com/google/android/update-binary
-done
 
 rm -rf META-INF/
 
@@ -62,8 +16,8 @@ cd ..
 if [ ! -f aroma.zip ]; then
   wget -O aroma.zip 'http://forum.xda-developers.com/devdb/project/dl/?id=286&task=get'
 fi
-unzip aroma.zip META-INF/com/google/android/update-binary{,-installer}
+unzip aroma.zip META-INF/com/google/android/update-binary
 zip DualBootUtilities-${VERSION}.zip \
-    META-INF/com/google/android/update-binary{,-installer}
+    META-INF/com/google/android/update-binary
 
 rm -rf META-INF/
