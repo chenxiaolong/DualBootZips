fea3f4ce51e720c0faa57905d5fde6b656c15ada
Xiao-Long Chen <chenxiaolong@cxl.epac.to>
2015-06-12 22:50:27 -0400

    Switch to mbtool-based wiping

--- create-zips.sh
+++ create-zips.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-cd "$(dirname ${0})"
-
-UPDATE_BINARY=""
+cd "$(dirname "${0}")"
 
 rm -rf META-INF/
 
-cd DualBootUtilities
-VERSION=$(cat META-INF/com/google/android/aroma-config | \
-            sed -n 's/^.*ini_set(.*\"rom_version\",.*\"\(.*\)\".*$/\1/p')
-rm -f ../DualBootUtilities-${VERSION}.zip
-zip -r ../DualBootUtilities-${VERSION}.zip *
-cd ..
+pushd DualBootUtilities/template/
+version=$(sed -n 's/^.*ini_set(.*\"rom_version\",.*\"\(.*\)\".*$/\1/p' \
+              META-INF/com/google/android/aroma-config.in)
+popd
+
+zip_file="$(readlink -f DualBootUtilities-"${version}".zip)"
+
+pushd DualBootUtilities/
+rm -f "${zip_file}"
+zip -r "${zip_file}" ./*
+popd
 
 if [ ! -f aroma.zip ]; then
-  wget -O aroma.zip 'http://forum.xda-developers.com/devdb/project/dl/?id=286&task=get'
+    wget -O aroma.zip 'http://forum.xda-developers.com/devdb/project/dl/?id=286&task=get'
 fi
-unzip aroma.zip META-INF/com/google/android/update-binary
-zip DualBootUtilities-${VERSION}.zip \
-    META-INF/com/google/android/update-binary
 
-rm -rf META-INF/
+aroma="$(readlink -f aroma.zip)"
+
+tempdir="$(mktemp -d)"
+mkdir -p "${tempdir}/template/"
+pushd "${tempdir}/template/"
+unzip "${aroma}" META-INF/com/google/android/update-binary
+cd ..
+zip "${zip_file}" template/META-INF/com/google/android/update-binary
+popd
+rm -rf "${tempdir}"
